9f11c1056ef5b5aa05510c7c932a2a04223b0a62
Edgar Caballero <ecaballeromora@outlook.com>
2023-11-11 02:10:09 +0100

    creo la clase User

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,15 +1,6 @@
-export class User {
-    public id_user!: number;
-    public name: string;
-    public last_name: string;
-    public email: string;
-    public url: string;
-    public photo!: string;
-    public password!: string;
-    constructor(name: string, last_name: string, email: string, url: string){
-        this.name = name;
-        this.last_name = last_name;
-        this.email = email;
-        this.url = url;
-    }
+export class User{
+    public id_user: string = "";
+    public photo: string = "";
+    public password: string = "";
+    constructor(public name: string, public surname: string, public email: string, public url: string){}
 }
