0c4e4b89166b010f252acfb46e0722ec5254ba69
Edgar Caballero <ecaballeromora@outlook.com>
2023-11-12 22:02:12 +0100

    archivos borrados

--- src/models/user.ts
+++
@@ -1,6 +0,0 @@
-export class User{
-    public id_user: string = "";
-    public photo: string = "";
-    public password: string = "";
-    constructor(public name: string, public surname: string, public email: string, public url: string){}
-}
