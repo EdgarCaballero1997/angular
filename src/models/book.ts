a25ab4265c66f3545bc68ec15ee6eabfbb4f144f
Edgar Caballero <ecaballeromora@outlook.com>
2023-11-14 19:25:50 +0100

    intento fallido de realizar los retos de servicios y routing avanzado

--- src/models/book.ts
+++
@@ -1,5 +0,0 @@
-export class Book {
-    constructor(public id_book: string = '', public id_user: string = '', public title: string, public type: string, public author: string, public price: string, public photo: string){
-        //Añado los parametros "id_book", "id_user" y "price" como tipo string, pero posteriormente los transformaré en Number.
-    }
-}
