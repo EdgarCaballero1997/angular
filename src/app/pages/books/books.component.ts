241428dd3456e1577f996e6319f3aab5090b2391
Edgar Caballero <ecaballeromora@outlook.com>
2023-11-23 01:19:14 +0100

    reto dia5 angular

--- src/app/pages/books/books.component.ts
+++ src/app/pages/books/books.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Book } from 'src/models/book';
 import { BooksService } from 'src/app/shared/books.service';
+import { CommunicationService } from 'src/app/shared/communication.service';
 
 @Component({
   selector: 'app-books',
@@ -19,24 +20,71 @@ export class BooksComponent implements OnInit {
   numUser: number = +this.precio;
   foto: string = "";
   librosPorDefecto: Book[] = [];
-  constructor(private booksService: BooksService) {
+  coincidenciaEncontrada: boolean = false;
+
+  constructor(private booksService: BooksService, private communicationService: CommunicationService) {
+
   }
-  ngOnInit(){
-    this.booksService.getAll().subscribe((books: Book[]) => {
-      this.librosPorDefecto = books;
-    });
+
+  onFiltroIdChange(filtroId: string) {
+    console.log('Filtro ID Cambiado:', filtroId);
+    if (filtroId) {
+      this.librosPorDefecto = this.booksService.searchBooks(filtroId);
+    } else {
+      this.booksService.getAll().subscribe((books: Book[]) => {
+        this.librosPorDefecto = books;
+      });
+    }
   }
+
+  ngOnInit() {
+    this.booksService.getAll().subscribe(
+      (books: Book[]) => {
+        this.librosPorDefecto = books;
+      },
+      (error) => {
+        console.error('Error al obtener libros:', error);
+      }
+    );
+
+    this.communicationService.filtroId$.subscribe(
+      (filtroId: string) => {
+        if (filtroId) {
+          const resultados = this.booksService.searchBooks(filtroId);
+          this.coincidenciaEncontrada = resultados.length > 0;
+          this.librosPorDefecto = resultados;
+        } else {
+          this.booksService.getAll().subscribe(
+            (books: Book[]) => {
+              this.coincidenciaEncontrada = false;
+              this.librosPorDefecto = books;
+            },
+            (error) => {
+              console.error('Error al obtener libros después de limpiar el filtro:', error);
+            }
+          );
+        }
+      },
+      (error) => {
+        console.error('Error en la suscripción al filtroId$:', error);
+      }
+    );
+  }
+
   infoInputs() {
     const nuevoLibro = new Book(this.id_book, this.id_user, this.titulo, this.genero, this.autor, this.precio, this.foto);
     this.booksService.add(nuevoLibro);
     alert("¡Libro añadido!");
   }
+
   alert(){
     alert("Copia y pega una de estas rutas:\n../../../assets/img/pasionIndia.jpg\n../../../assets/img/juanramonrallo.jpeg\n../../../assets/img/kenfollet.jpg\n");
   }
+
   borrarLocalStorage() {
     localStorage.removeItem('librosPorDefecto');
   }
+
   onLibroEliminado(index: number) {
     this.booksService.deleteBook(index);
   }
--- src/app/pages/update-book/update-book.component.ts
+++ src/app/pages/update-book/update-book.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Book } from 'src/models/book';
+import { BooksService } from 'src/app/shared/books.service';
 
 @Component({
   selector: 'app-update-book',
@@ -7,4 +9,37 @@ import { Component } from '@angular/core';
 })
 export class UpdateBookComponent {
 
+  libro: Book = new Book('', '', '', '', '', '', ''); // Objeto que contendrá los datos del libro
+
+  constructor(private booksService: BooksService){
+
+  }
+
+  actualizarLibro() {
+    // Obtén el libro actual para preservar los datos no modificados
+    const libroActual = this.booksService.getOne(this.libro.id_book);
+
+    if (libroActual) {
+      // Actualiza solo las propiedades modificadas
+      const libroActualizado = new Book(
+        this.libro.id_book,
+        this.libro.id_user || libroActual.id_user,
+        this.libro.title || libroActual.title,
+        this.libro.type || libroActual.type,
+        this.libro.author || libroActual.author,
+        this.libro.price || libroActual.price,
+        this.libro.photo || libroActual.photo
+      );
+
+      const actualizacionExitosa = this.booksService.edit(libroActualizado);
+
+      if (actualizacionExitosa) {
+        alert("¡Libro actualizado!");
+      } else {
+        alert("No se pudo actualizar el libro. Verifica el ID del libro.");
+      }
+    } else {
+      alert("Libro no encontrado. Verifica el ID del libro.");
+    }
+  }
 }
